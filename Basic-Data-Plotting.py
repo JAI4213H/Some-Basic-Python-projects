c1b01e046fd838122a504c58ba1be85d00eafb26
Jai Tanwar <106313552+JAI4213H@users.noreply.github.com>
2026-08-19 17:35:23 +0530

    Refactor input fields and update graph plotting logic
    Refactor column input handling to use ttk.Combobox for better user experience. Update graph plotting logic to utilize new input method.

--- Basic-Data-Plotting.py
+++ Basic-Data-Plotting.py
@@ -3,19 +3,22 @@ import tkinter as tk
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from tkinter import ttk
 
-submittedcolumns = []
+
+
 data_location = None
 df = None
-
+numeric_list = None
 if data_location == None:
     def temp_submissions():
-        global data_location,df
+        global data_location,df,numeric_list
         data_location = data_loc.get()
         
         try: 
             data = pd.read_csv(f"{data_location}")
             df = pd.DataFrame(data=data)
+            numeric_list = list(df.select_dtypes(include=[float,int]).columns)
             temp_submission.destroy()
         except FileNotFoundError:
             
@@ -35,35 +38,37 @@ if data_location == None:
 
 def submitstuff():
 
-    submittedcolumns.clear()
+    
 
-    submittedcolumns.append(column1.get())
-    submittedcolumns.append(column2.get())
+    
 
-    print("Both the features are:  ", submittedcolumns)
+    print("Both the features are:  ", storing_string_for_column1.get(),storing_string_for_column2.get())
     makegraph(graph_frame)
     graph_frame.grid()
 
 
 def makegraph(frame):
 
-    if len(submittedcolumns) == 2:
+    
 
-        x = df[submittedcolumns[0]]
-        y = df[submittedcolumns[1]]
-        figure = plt.Figure(figsize=(5, 4))
-        ax = figure.add_subplot(111)
-        type_graph = getattr(ax , column3.get())
-        
-        type_graph(x, y)
+    x = df[storing_string_for_column1.get()]
+    y = df[storing_string_for_column2.get()]
+    
+    for i in frame.winfo_children():
+        i.destroy()
+    figure = plt.Figure(figsize=(5, 4))
+    ax = figure.add_subplot(111)
+    type_graph = getattr(ax , storing_string_for_column3.get())
+    
+    type_graph(x, y)
 
-        ax.set_xlabel(submittedcolumns[0])
-        ax.set_ylabel(submittedcolumns[1])
-        canvas = FigureCanvasTkAgg(figure, master=frame)
+    ax.set_xlabel(storing_string_for_column1.get())
+    ax.set_ylabel(storing_string_for_column2.get())
+    canvas = FigureCanvasTkAgg(figure, master=frame)
 
-        canvas.draw()
+    canvas.draw()
 
-        canvas.get_tk_widget().pack(fill="both",expand=True)
+    canvas.get_tk_widget().pack(fill="both",expand=True)
 
 
 root = tk.Tk()
@@ -71,13 +76,34 @@ root.geometry("800x500")
 root.columnconfigure(1, weight=1)
 root.rowconfigure(0, weight=1)
 
+## textVARIABLESSS----------
+storing_string_for_column3 = tk.StringVar()
+storing_string_for_column2 = tk.StringVar()
+storing_string_for_column1 = tk.StringVar()
+#---------------------------------
+#texts
 text1 = tk.Label(root, text="Enter the column names")
 label1 = tk.Label(root, text="Column 1")
-column1 = tk.Entry(root)
 label2 = tk.Label(root, text="Column 2")
-column2 = tk.Entry(root)
 label3 = tk.Label(root, text="Type of Graph")
-column3 = tk.Entry(root)
+#-------------------------------------------
+
+
+column1 = ttk.Combobox(root,textvariable=storing_string_for_column1)
+column2 = ttk.Combobox(root,textvariable=storing_string_for_column2)
+column3 = ttk.Combobox(root,textvariable=storing_string_for_column3)
+
+
+list_of_plots = ['scatter','bar','plot']
+
+
+column1['state'] = 'readonly'
+column1['values'] = numeric_list
+column2['state'] = 'readonly'
+column2['values'] = numeric_list
+column3['state'] = 'readonly'
+column3['values'] = list_of_plots
+
 text1.grid(row=0, column=0, columnspan=2, padx=10, pady=5)
 
 label1.grid(row=1, column=0, padx=10, pady=3)

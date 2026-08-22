3648ceba20ae261e211f24fcaaec661df1c17f26
Jai Tanwar <106313552+JAI4213H@users.noreply.github.com>
2026-08-22 17:51:13 +0530

    Add login functionality to authentication app

--- tkinter-auth-app.py
+++ tkinter-auth-app.py
@@ -7,6 +7,7 @@ connect = sql.connect(host = "localhost",username="root",password='',database="w
 cursor = connect.cursor()
 query = "INSERT INTO userdata (username,password) VALUES (%s,%s)"
 check_query = "SELECT username FROM userdata WHERE username = %s"
+login_query = "SELECT username FROM userdata WHERE username = %s AND password = %s"
 
 option_window = tk.Tk()
 option_window.geometry("400x180")
@@ -35,8 +36,8 @@ def CreateAcountWindow():
             print("Somehting Went Wrong!!!",e)
 
         
-    option_window.destroy()
-    create_window = tk.Tk()
+    option_window.withdraw()
+    create_window = tk.Toplevel(option_window)
     create_window.columnconfigure(0,weight=1)
     create_window.columnconfigure(1,weight=2)
     create_window.geometry("400x200")
@@ -56,13 +57,53 @@ def CreateAcountWindow():
     username_entry.grid(row=1,column=1)
     pass_entry.grid(row=2,column=1)
     sumbit_butn.grid(row=3,column=1)
+
+
+
+def LoginWindow():
     
-    create_window.mainloop()
+    def login():
+        username = username_entry.get()
+        password = pass_entry.get()
+        values = (username,password)
+        
+        try:
+            cursor.execute(login_query,values)
+            login_result = cursor.fetchone()
+            
+            if login_result is not None:
+                confirmLable.config(text="Login Successful!",foreground="green")
+            else:
+                confirmLable.config(text="INVALID USERNAME OR PASSWORD!!!!!!!",foreground="red")
+        except Exception as e:
+            print("Something Went Wrong!!!",e)
     
     
+    option_window.withdraw()
+    login_window = tk.Toplevel(option_window)
+    login_window.columnconfigure(0,weight=1)
+    login_window.columnconfigure(1,weight=2)
+    login_window.geometry("400x200")
+    
+    lable1 = ttk.Label(login_window,text="Username:")
+    lable2 = ttk.Label(login_window,text="Password")
+    username_entry = ttk.Entry(login_window)
+    pass_entry = ttk.Entry(login_window,show="*")
+    login_butn = ttk.Button(login_window,text="Login",command=login)
+    confirmLable = ttk.Label(login_window,text="")
+    
+    username_entry.focus()
+    
+    confirmLable.grid(row=4,column=1)
+    lable1.grid(row=1,column=0,ipadx=20,ipady=5)
+    lable2.grid(row=2,column=0,ipadx=20,ipady=5)
+    username_entry.grid(row=1,column=1)
+    pass_entry.grid(row=2,column=1)
+    login_butn.grid(row=3,column=1)
+
 
 create_button = ttk.Button(option_window,text="Create Account",command=CreateAcountWindow)
-login_button = ttk.Button(option_window,text="Login")
+login_button = ttk.Button(option_window,text="Login",command=LoginWindow)
 create_button.grid(row=1,column=1,ipadx=30,ipady=15,padx=30,pady=4)
 login_button.grid(row=2,column=1,ipadx=30,ipady=15,padx=10,pady=4)
 

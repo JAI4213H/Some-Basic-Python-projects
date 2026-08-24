78205ea2f5502d8373aef591b8759616f4517d83
Jai Tanwar <106313552+JAI4213H@users.noreply.github.com>
2026-08-24 20:02:00 +0530

    Refactor GUI layout and improve error handling

--- Basic-Data-Plotting.py
+++ Basic-Data-Plotting.py
@@ -1,4 +1,3 @@
-
 import tkinter as tk
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -11,8 +10,10 @@ data_location = None
 df = None
 numeric_list = None
 if data_location == None:
+
     def temp_submissions():
         global data_location,df,numeric_list
+
         data_location = data_loc.get()
         
         try: 
@@ -20,19 +21,27 @@ if data_location == None:
             df = pd.DataFrame(data=data)
             numeric_list = list(df.select_dtypes(include=[float,int]).columns)
             temp_submission.destroy()
+
         except FileNotFoundError:
-            
             print("Your input of filename isnt correct")
         
     
 
-    temp_submission = tk.Tk()
-    tk.Label(temp_submission,text="Enter name of the dataset or its location").pack(padx=10,pady=2)
+    temp_submission = tk.Toplevel(root)
+
+    tk.Label(
+        temp_submission,
+        text="Enter name of the dataset or its location"
+    ).pack(padx=10,pady=2)
 
     data_loc = tk.Entry(temp_submission)
     data_loc.pack(side=tk.LEFT,padx=10,pady=3)
-    tk.Button(temp_submission,text="Submit",command=temp_submissions).pack()
-    temp_submission.mainloop()
+
+    tk.Button(
+        temp_submission,
+        text="Submit",
+        command=temp_submissions
+    ).pack()
 
 
 
@@ -42,7 +51,12 @@ def submitstuff():
 
     
 
-    print("Both the features are:  ", storing_string_for_column1.get(),storing_string_for_column2.get())
+    print(
+        "Both the features are:  ",
+        storing_string_for_column1.get(),
+        storing_string_for_column2.get()
+    )
+
     makegraph(graph_frame)
     graph_frame.grid()
 
@@ -56,23 +70,32 @@ def makegraph(frame):
     
     for i in frame.winfo_children():
         i.destroy()
+
     figure = plt.Figure(figsize=(5, 4))
     ax = figure.add_subplot(111)
-    type_graph = getattr(ax , storing_string_for_column3.get())
-    
+
+    type_graph = getattr(
+        ax,
+        storing_string_for_column3.get()
+    )
     type_graph(x, y)
 
     ax.set_xlabel(storing_string_for_column1.get())
     ax.set_ylabel(storing_string_for_column2.get())
-    canvas = FigureCanvasTkAgg(figure, master=frame)
+    canvas = FigureCanvasTkAgg(
+        figure,
+        master=frame
+    )
 
     canvas.draw()
 
-    canvas.get_tk_widget().pack(fill="both",expand=True)
-
-
+    canvas.get_tk_widget().pack(
+        fill="both",
+        expand=True
+    )
 root = tk.Tk()
 root.geometry("800x500")
+
 root.columnconfigure(1, weight=1)
 root.rowconfigure(0, weight=1)
 
@@ -81,17 +104,43 @@ storing_string_for_column3 = tk.StringVar()
 storing_string_for_column2 = tk.StringVar()
 storing_string_for_column1 = tk.StringVar()
 #---------------------------------
+
+
 #texts
-text1 = tk.Label(root, text="Enter the column names")
-label1 = tk.Label(root, text="Column 1")
-label2 = tk.Label(root, text="Column 2")
-label3 = tk.Label(root, text="Type of Graph")
+text1 = tk.Label(
+    root,
+    text="Enter the column names"
+)
+
+label1 = tk.Label(
+    root,
+    text="Column 1"
+)
+
+label2 = tk.Label(
+    root,
+    text="Column 2"
+)
+
+label3 = tk.Label(
+    root,
+    text="Type of Graph"
+)
 #-------------------------------------------
+column1 = ttk.Combobox(
+    root,
+    textvariable=storing_string_for_column1
+)
 
+column2 = ttk.Combobox(
+    root,
+    textvariable=storing_string_for_column2
+)
 
-column1 = ttk.Combobox(root,textvariable=storing_string_for_column1)
-column2 = ttk.Combobox(root,textvariable=storing_string_for_column2)
-column3 = ttk.Combobox(root,textvariable=storing_string_for_column3)
+column3 = ttk.Combobox(
+    root,
+    textvariable=storing_string_for_column3
+)
 
 
 list_of_plots = ['scatter','bar','plot']
@@ -104,40 +153,69 @@ column2['values'] = numeric_list
 column3['state'] = 'readonly'
 column3['values'] = list_of_plots
 
-text1.grid(row=0, column=0, columnspan=2, padx=10, pady=5)
+text1.grid(
+    row=0,
+    column=0,
+    columnspan=2,
+    padx=10,
+    pady=5
+)
+label1.grid(
+    row=1,
+    column=0,
+    padx=10,
+    pady=3)
+column1.grid(
+    row=1,
+    column=1,
+    padx=10,
+    pady=3)
+label2.grid(
+    row=2,
+    column=0,
+    padx=10,
+    pady=3)
 
-label1.grid(row=1, column=0, padx=10, pady=3)
-column1.grid(row=1, column=1, padx=10, pady=3)
-label2.grid(row=2, column=0, padx=10, pady=3)
-column2.grid(row=2, column=1, padx=10, pady=3)
+column2.grid(
+    row=2,
+    column=1,
+    padx=10,
+    pady=3)
 
-label3.grid(row=3, column=0, padx=10, pady=3)
-column3.grid(row=3, column=1, padx=10, pady=3)
+label3.grid(
+    row=3,
+    column=0,
+    padx=10,
+    pady=3)
+
+column3.grid(
+    row=3,
+    column=1,
+    padx=10,
+    pady=3)
 
 submit_button = tk.Button(
     root,
     text="Submit",
-    command=submitstuff
-)
+    command=submitstuff)
 
 submit_button.grid(
     row=4,
     column=0,
     columnspan=2,
-    pady=20
-)
+    pady=20)
+
 
 graph_frame = tk.Frame(root)
 
-graph_frame.grid( column=2,row=0,rowspan=4,sticky="nsew"
+graph_frame.grid(
+    column=2,
+    row=0,
+    rowspan=4,
+    sticky="nsew"
 )
+
 graph_frame.grid_remove()
-
-
-
-
-
-
 
 
 root.mainloop()

288bb5614f50cd26543858a48e87315a3f7ee3e1
Jai Tanwar <106313552+JAI4213H@users.noreply.github.com>
2026-09-05 23:58:09 +0530

    Add token generation functionality in TokenPilottttt.py
    Implement a token generation loop using a language model. And the user can decide if they want to add this word or not

---
+++ TokenPilottttt.py
@@ -0,0 +1,27 @@
+import torch
+from transformers import set_seed
+from transformers import AutoTokenizer
+from transformers import pipeline
+from transformers import AutoModelForCausalLM
+set_seed(10)
+prompt = "My throat becomes heavy when i see her with another guy"
+tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2-0.5B")
+model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2-0.5B")
+
+x = 1
+while True:
+    if x == 0:
+        break
+    tokenized_ids = tokenizer(prompt,return_tensors="pt").input_ids
+    output = model(tokenized_ids).logits[0,-1]
+    mp_tensor = output.argmax()
+    
+    top_10 = torch.topk(output.softmax(dim=0),10)
+    
+    for i,j in zip(top_10.values,top_10.indices):
+        print(f"{tokenizer.decode(j.item()):<10} {i.item():.3%}")
+    print("------------------------------------")
+    x = int(input("Enter 0 or 1"))
+    
+    prompt += tokenizer.decode(output.argmax().item())
+    print(f"New prompt is {prompt}")

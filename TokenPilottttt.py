46ca16b597e9a6aedc8663fb35248255944d36b2
Jai Tanwar <106313552+JAI4213H@users.noreply.github.com>
2026-09-06 11:15:48 +0530

    Allow user input for prompt in TokenPilottttt.py

--- TokenPilottttt.py
+++ TokenPilottttt.py
@@ -4,7 +4,8 @@ from transformers import AutoTokenizer
 from transformers import pipeline
 from transformers import AutoModelForCausalLM
 set_seed(10)
-prompt = "My throat becomes heavy when i see her with another guy"
+x = str(input("Enter the Line you want to complete: "))
+prompt = x
 tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2-0.5B")
 model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2-0.5B")
 
